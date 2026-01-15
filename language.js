bb8871013e41a3d881f6f491529b310784470a1c
mertsyh <146176740+mertsyh@users.noreply.github.com>
2026-01-15 16:43:14 +0300

    Update language.js

--- language.js
+++ language.js
@@ -7,21 +7,21 @@ function setLanguage(lang) {
   localStorage.setItem("lang", lang);
 }
 
-// Sayfa yüklendiğinde
+
 window.addEventListener("load", () => {
   const savedLang = localStorage.getItem("lang") || "tr";
   setLanguage(savedLang);
 
-  // Toggle varsa durumunu ayarla
   if (languageToggle) {
     languageToggle.checked = savedLang === "en";
   }
 });
 
-// Toggle varsa dinle
+
 if (languageToggle) {
   languageToggle.addEventListener("change", () => {
     const lang = languageToggle.checked ? "en" : "tr";
     setLanguage(lang);
   });
 }
+
